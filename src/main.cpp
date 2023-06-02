65c2421688fa0b0e9e63f051f51ac20619b6f3e5
ElMariuso <thiry.marius@gmail.com>
2023-06-02 12:38:38 +0200

    Basic main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 12:08:11 by root              #+#    #+#             */
-/*   Updated: 2023/06/02 12:32:53 by root             ###   ########.fr       */
+/*   Updated: 2023/06/02 12:38:12 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,18 @@
 
 int main(int argc, char **argv)
 {
-    (void)argc;
     (void)argv;
+    try
+    {
+        if (argc < 2)
+            throw (std::runtime_error("Not enough arguments."));
+        else if (argc > 2)
+            throw (std::runtime_error("Too many arguments."));
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return (1);
+    }
     return (0);
 }
