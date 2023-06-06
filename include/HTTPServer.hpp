c083d8a754914c16cf3b2c73459ff99491105500
ElMariuso <thiry.marius@gmail.com>
2023-06-07 00:39:50 +0200

    Basic setup of socket

--- include/HTTPServer.hpp
+++ include/HTTPServer.hpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 16:47:13 by root              #+#    #+#             */
-/*   Updated: 2023/06/02 13:35:48 by root             ###   ########.fr       */
+/*   Updated: 2023/06/07 00:07:26 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,23 @@
 /* Includes ***************************************************************** */
 
 # include <iostream>
+# include <sys/socket.h>
+# include <sys/types.h>
+
+# include <netinet/in.h>
+# include <cstring>
 
 /* Includes.hpp ************************************************************* */
 
 # include "ConfigParser.hpp"
+
+/* Structure **************************************************************** */
+
+typedef struct s_socket
+{
+    int                 fd;
+    struct sockaddr_in  addr;
+}   t_socket;
 
 /* Class ******************************************************************** */
 
@@ -34,8 +47,17 @@ class HTTPServer: public ConfigParser
         /* Destructor */
         ~HTTPServer();
 
+        /* Utils */
+        int                     setup(int index);
+        struct sockaddr_in      setDefaultAddr(int index);
+        int                     process();
+
+        /* Getters */
+        std::vector<t_socket>   getSockets();
+
     /* Attributes *************************************************************** */
     private:
+        std::vector<t_socket>   sockets;
 };
 
 #endif
--- src/HTTPServer.cpp
+++ src/HTTPServer.cpp
@@ -6,19 +6,87 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 13:36:01 by root              #+#    #+#             */
-/*   Updated: 2023/06/02 13:37:27 by root             ###   ########.fr       */
+/*   Updated: 2023/06/07 00:37:10 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/HTTPServer.hpp"
 
 /* Constructor */
-HTTPServer::HTTPServer() {}
+HTTPServer::HTTPServer(): ConfigParser()
+{
+    int         ret;
+    std::string error_message;
+
+    for (std::size_t i = 0; i != this->port.size(); ++i)
+    {
+        ret = this->setup(i);
+        if (ret != 0)
+        {
+            error_message = "During setting up server: " + std::string(strerror(ret));
+            throw (std::runtime_error(error_message));
+        }
+    }
+    ret = this->process();
+}
 
 HTTPServer::HTTPServer(std::string path): ConfigParser(path)
 {
+    int         ret;
+    std::string error_message;
 
+    for (std::size_t i = 0; i != this->port.size(); ++i)
+    {
+        ret = this->setup(i);
+        if (ret != 0)
+        {
+            error_message = "During setting up server: " + std::string(strerror(ret));
+            throw (std::runtime_error(error_message));
+        }
+    }
+    ret = this->process();
 }
 
 /* Destructor */
 HTTPServer::~HTTPServer() {}
+
+/* Utils */
+int HTTPServer::setup(int index)
+{
+    t_socket    new_elem;
+    int         error;
+
+    new_elem.fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (new_elem.fd < 0)
+    {
+        error = errno;
+        return (error);
+    }
+    new_elem.addr = this->setDefaultAddr(index);
+    if (bind(new_elem.fd, (struct sockaddr *)&new_elem.addr, sizeof(new_elem.addr)) < 0)
+    {
+        error = errno;
+        return (error);
+    }
+    this->sockets.push_back(new_elem);
+    return (0);
+}
+
+struct sockaddr_in HTTPServer::setDefaultAddr(int index)
+{
+    struct sockaddr_in  ret;
+
+    memset((char *)&ret, 0, sizeof(ret));
+    ret.sin_family = AF_INET;
+    ret.sin_port = htons(this->port[index]);
+    // ret.sin_addr.s_addr = htonl(this->host);
+    return (ret);
+}
+
+int HTTPServer::process()
+{
+    return (0);   
+}
+
+/* Getters */
+std::vector<t_socket> HTTPServer::getSockets() { return (this->sockets); }
